e050a1c41c248370e0f973ca51b9339df79e9369
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2024-09-11 13:48:28 +0000

    add: conversionOperations

---
+++ conversion_operation.js
@@ -0,0 +1,79 @@
+
+// let name = "KRISHNA";
+// console.log(typeof name);
+// console.log(typeof(name));
+
+// let nameInNumber = Number(name);
+// console.log(nameInNumber);  // output: NaN
+
+// console.log("108"); // output: 108
+
+// "16" => 16
+// "16Ram" => NaN
+//  true => 1; false => 0
+
+// ----
+
+// let isLoggedIn = "KRISHNA"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "KRISHNA" => true
+
+// ----
+
+// let num = 64;
+
+// let stringNum = String(num);
+
+// console.log(stringNum);
+// console.log(typeof(stringNum));
+
+
+// ----
+
+// let positiveNum = 16;
+// let negativeNum = -positiveNum;
+
+// console.log(negativeNum);
+
+// ----
+
+// let str1 = "HARE ";
+// let str2 = "KRISHNA";
+// let str3 = str1 + str2;
+
+// console.log(str3);
+
+// ----
+
+// console.log("1" + 6); // output: 16
+// console.log(1 + "6"); // output: 16
+// console.log("1" + 6 + 6); // output: 166, because: begining treated as str and followedit
+// console.log(1 + 6 + "6"); // output: 76, because: begining treated as num and then str
+
+// ----
+
+// console.log(+true); // output: 1, bcz: begining treated as num
+// console.log(+""); // output: 0, bcz: begining treated as num
+
+// ----
+
+// let num1 = 1, num2 = "RAM", num3 = true;
+
+// num1 = num2 = num3 = 2 + 3;
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+// ----
+
+// let num = 1, str = "RAM", bool = true;
+
+// console.log(num);
+// console.log(str);
+// console.log(bool);
