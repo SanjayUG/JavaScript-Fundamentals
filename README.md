ddcedafee20648330f5fade083007906b0991452
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2024-09-11 12:01:35 +0000

    add: variableDeclearing in js

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # JavaScript-Fundamentals
+<<<<<<< HEAD
+
+
+=======
 
 ## just for fun
+>>>>>>> 32e00d49ed2a165789c22520ed2d8f037cfd9747
---
+++ variable_declearing.js
@@ -0,0 +1,26 @@
+
+const id = 108;  // for constant variables
+
+let password = 12341234;  // declearing a variable with a block scope
+
+var name = "KRISHNA";  // declearing variable with function scope (to access it throughout program excution)
+
+email = "demo@gmail.com"; // 
+
+
+//id = 16;  //TypeError: Assignment to constant variable.
+
+// console.log(id);
+// console.log(password);
+// console.log(name);
+// console.log(email);
+
+
+// console.log(typeof id);
+// console.log(typeof password);
+// console.log(typeof name);
+// console.log(typeof email);
+
+/*
+    Always use `let` for declearing a variable for block scope.
+*/
