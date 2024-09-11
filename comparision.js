18f028fe99afeca128e7babf13eb0d082948fffd
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2024-09-11 14:09:31 +0000

    add: comparisions

---
+++ comparision.js
@@ -0,0 +1,32 @@
+
+// basics
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// ----
+
+// console.log("6" > 1); // output: true
+// console.log("06" > 1); // output: true
+
+// ----
+
+// console.log(null > 0); // output: false
+// console.log(null == 0); // output: false, bcz: equality(==) treated differently than comparator
+// console.log(null >= 0); // output: true, bcz: comparision convert null to num
+
+// console.log(undefined == 0); // output: false
+// console.log(undefined > 0); // output: flase
+// console.log(undefined < 0); // output: flase
+// console.log(undefined == null); // output: true, because: equlity loosly equal
+// console.log(undefined !== null); // output: true
+
+
+// ----
+
+//type strict check
+
+console.log("06" === 06);
