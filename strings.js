bd7bf660ab87d9e51b6d6db01a1459166f595ff4
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2024-09-11 15:01:10 +0000

    add: strings

---
+++ strings.js
@@ -0,0 +1,38 @@
+
+// const id = 64
+// const name = "KRISHNA"
+
+// console.log("Name:" + name + " " + "Id:" + id );
+
+// console.log(`Name: ${name} Id: ${id}`);
+
+// ----
+
+const url = new String("www.microsoft.com"); // here String is an object with (key, value) pair
+
+// console.log(url[0]); 
+// console.log(url.__proto__); // output: object i.e {}
+
+// console.log(url.length);
+// console.log(url.toUpperCase());
+// console.log(url.toLowerCase());
+// console.log(url.charAt(4));
+// console.log(url.indexOf('m'));
+
+// console.log(url.substring(0, 4));
+// console.log(url.substring(-1, 2)); // output: ww, because: if negative value then it starts from 0 index
+
+// console.log(url.slice(3, 7));
+// console.log(url.slice(-14, 6));
+
+// console.log(url.replace("com", "in"));
+// console.log(url.includes("ram")); // output: flase
+// console.log(url.split(".")); // output: [ 'www', 'microsoft', 'com' ]
+
+// ----
+
+// const name = "    HARE    KRISHNA  "; 
+
+// console.log(name.trim()); // removes white-spaces at begining and ending
+
+
