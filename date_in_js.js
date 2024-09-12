022d5b46c41b2193556440655d0d7c7eaf1c390c
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2024-09-12 05:51:27 +0000

    add: played with Date in javascript

---
+++ date_in_js.js
@@ -0,0 +1,59 @@
+
+// console.log(Date());
+
+// const date = new Date();
+
+// console.log(date);
+// console.log(date.toJSON());
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+// console.log(date.toLocaleString());
+
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+
+// console.log(typeof(date)); // output: object
+
+// ----
+
+// const createdDate1 = new Date(2024, 0, 16);
+// const createdDate2 = new Date(2024, 4, 16, 0, 24, 5, 108);
+
+// console.log(createdDate1.toString());
+// console.log(createdDate2.toString());
+
+// ----
+
+// const createdDate3 = new Date("2024-01-01");
+// const createdDate4 = new Date("01-01-2024");
+// console.log(createdDate3);
+// console.log(createdDate4);
+
+// console.log(createdDate3.toLocaleString());
+
+// ----
+
+const myTimeStamp = new Date();
+
+// console.log(myTimeStamp);
+// console.log(myTimeStamp.toLocaleString());
+
+// console.log(Math.floor(Date.now()/1000)); // Date.now returns the millsecond value
+
+// ----
+
+// const [month, date, year] = [myTimeStamp.getDate(), myTimeStamp.getMonth() + 1, myTimeStamp.getFullYear()]
+
+// console.log(month);
+// console.log(date);
+// console.log(year);
+
+// ----
+
+// myTimeStamp.toLocaleString('default', {
+//     weekday: "long"
+// })
+
+// console.log(myTimeStamp);
+
