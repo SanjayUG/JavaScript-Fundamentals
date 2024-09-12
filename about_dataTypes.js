4881132930a683755840ac3f2efcfc78365a9e44
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2024-09-12 06:27:58 +0000

    add: added some more content ot dataTypes

--- about_dataTypes.js
+++ about_dataTypes.js
@@ -14,14 +14,15 @@ let id = null; // object
 
 // console.table([typeof name, typeof age, typeof isLoggedIn, typeof city]);
 
-console.table([
-    {value: name, type:typeof name},
-    {value: age, type:typeof age},
-    {value: isLoggedIn, type:typeof isLoggedIn},
-    {value: city, type:typeof city},
-    {value: id, type:typeof id},
-])
+// console.table([
+//     {value: name, type:typeof name},
+//     {value: age, type:typeof age},
+//     {value: isLoggedIn, type:typeof isLoggedIn},
+//     {value: city, type:typeof city},
+//     {value: id, type:typeof id},
+// ])
 
+// ----
 
 // PRIMITIVE dataType
 
@@ -31,13 +32,49 @@ console.table([
 // 4. boolean => true/false
 // 5. null => standalone value
 // 6. undefined 
-// 7. symbol => unique
+// 7. symbol => unique and immutable, used to identify object
 
 
 // Non Primitive dataType
 
 // 1. array
-// 2. object
+// 2. objects
 // 3. function
-// 4. date
 
+// ----
+
+// const registerId1 = Symbol("111");
+// const registerId2 = Symbol("111");
+
+// console.log(registerId1 === registerId2);
+
+// ----
+
+// const bigNumber = 3456543576654356754n; // use 'n' at end for bigInt
+// const bigNum = BigInt(123456789987654321)
+// console.log(bigNumber);
+// console.log(bigNum);
+
+// ----
+
+// const fruits = ["mango", "orange", "tomato", "blue-beary"]
+
+// console.log(fruits);
+
+
+// const student = {
+//     id: 16,
+//     name: "KRISHNA",
+//     age: 16
+// }
+
+// console.log(student);
+// console.log(typeof(student));
+
+
+const myFunction = function() {
+    console.log("HAREKRISHNA".includes("HARE"));
+}
+
+myFunction();
+console.log(typeof myFunction);
