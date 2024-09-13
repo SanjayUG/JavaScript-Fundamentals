6b0dcbcf3119b55ad160bba4820c2c4bd114c5b3
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2024-09-13 06:24:36 +0000

    add: about functions

---
+++ function.js
@@ -0,0 +1,80 @@
+
+// function chant() {
+//     console.log("HARE");
+//     console.log("KRISHNA");
+//     console.log("HARE");
+//     console.log("KRISHNA");
+//     console.log("KRISHNA");
+//     console.log("KRISHNA");
+//     console.log("HARE");
+//     console.log("HARE");
+//     console.log("HARE");
+//     console.log("RAMA");
+//     console.log("HARE");
+//     console.log("RAMA");
+//     console.log("RAMA");
+//     console.log("RAMA");
+//     console.log("HARE");
+//     console.log("HARE");
+    
+// }
+
+// chant();
+
+// function sum(num1, num2) {
+//     return num1 + num2;
+// }
+
+// const result = sum(1, 2);
+// console.log(result);
+
+// ----
+
+// function loginUser(username) {
+//     if(!username){
+//         console.log("PLease enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+
+// console.log(loginUser("KRISHNA"));
+// console.log(loginUser(""));
+
+// ----
+
+// function firstnLastValue(num1, ...nums) {
+//     console.log(`firstValue ${num1} and lastValue ${nums[nums.length - 1]}`);
+    
+//     return;
+// }
+
+// console.log(firstnLastValue(1, 2, 3, 8, 9, 16));
+
+// ----
+
+// const user = {
+//     username: "KRISHNA",
+//     price: 199
+// }
+
+// function handleUser(anyUser) {
+//     console.log(`Username is ${anyUser.username} and price is ${anyUser.price}`);
+// }
+
+// handleUser(user)
+// handleUser({
+//     username: "RAM",
+//     price: 200
+// })
+
+// ----
+
+// const arr = [10, 20, 30, 40];
+
+// function firstElement(anyArray) {
+//     return anyArray[0];
+// }
+
+// console.log(firstElement(arr));
+// console.log(firstElement([2, 3, 4, 7, 16, 16, 108]));
