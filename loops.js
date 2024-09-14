25908073a1daf0fb20c83a45cd6a46da5366f6ed
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2024-09-14 16:13:25 +0000

    add: added forEach loop

---
+++ loops.js
@@ -0,0 +1,7 @@
+
+const coding_languages = ["c", "cpp", "java", "javascript", "python"];
+
+coding_languages.forEach(function(currValue, index, array) {
+    console.log(`${currValue}, ${index}, ${array}`);
+    
+})
