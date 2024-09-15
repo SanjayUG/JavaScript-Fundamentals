7f48dbb4ffdfc84998dc2170bb518a2960a78ccb
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2024-09-15 09:03:11 +0000

    add: some more about for...of loop

--- loops.js
+++ loops.js
@@ -191,16 +191,27 @@
 
 // ----
 
-const map = new Map();
+//const map = new Map();
 
-map.set('IN', "India");
-map.set('USA', "United States of America");
-map.set('Fr', "France");
-map.set('IN', "India");
+//map.set('IN', "India");
+//map.set('USA', "United States of America");
+//map.set('Fr', "France");
+//map.set('IN', "India");
 
 //console.log(map);
 
-for (const [key, value] of map) {
-    console.log(key, ':-', value);
+//for(const [key, value] of map) {
+//    console.log(key, ':-', value);
+//}
+
+// ----
+
+const myObject = {
+    obj1: 'pen',
+    obj2: 'pencil'
 }
 
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value);
+                
+}
