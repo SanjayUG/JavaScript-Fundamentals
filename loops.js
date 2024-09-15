cfb5ad1c68640433a7d553eeaf2134297ff1362e
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2024-09-15 05:01:05 +0000

    add: about for loop

--- loops.js
+++ loops.js
@@ -1,4 +1,64 @@
 
+// for
+
+//for (let i = 0; i <= 10; i++) {
+//    const j = i;
+//    if (j == 7) {
+//        console.log("its talla(7) for reason");
+//    }
+//    console.log(j);
+//}       
+
+// ----
+
+//for(let i = 1; i <= 10; i++) {
+//    console.log(`Outer: ${i}`);
+//    for(let j = 1; j <= 10; j++) {
+//        console.log(`Inner: ${j}`);
+//        console.log(i + '*' + j + ' = ' + i*j );
+//    }
+//    console.log("\n");
+//
+//}
+
+// ----
+
+//let coding_languages = ["C", "CPP", "Java", "JavaScript"];
+
+//console.log(coding_languages.length);
+
+//for(let i=0; i<coding_languages.length; i++) {
+//    const element = coding_languages[i];
+//    console.log(element);
+//}
+
+// ----
+
+// break and continue
+
+//for(let i=1; i<=10; i++) {
+//    if(i==7) {
+//        console.log(`talla ${i} detected`);
+//        break;
+//    }
+//    console.log(`i: ${i}`);
+
+//}
+
+// ----
+
+//for(let i=1; i<= 10; i++) {
+//    if (i == 6) {
+//        console.log(`6 found`);
+//        continue;
+//    }
+//    console.log(`i: ${i}`);
+                                   
+//}
+
+
+// ---------------------------------------------------------------------
+
 // forEach
 
 // const coding_languages = ["c", "cpp", "java", "javascript", "python"];
